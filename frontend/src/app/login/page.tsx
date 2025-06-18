33f7a932e4e73f0c0bc62369047adf2368c71f2f
Varun Nukala <varun.k.nukala@gmail.com>
2025-06-18 18:46:32 -0500

    fix: wrap login page in suspense boundary

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,5 +1,10 @@
 import { LoginPage } from "@/lib/components/login-page";
+import { Suspense } from "react";
 
-export default function LogInRoute() {
-  return <LoginPage />;
+export default function Page() {
+  return (
+    <Suspense>
+      <LoginPage />
+    </Suspense>
+  );
 } 
